5ca9c231c595b6bf94c74e71d96492f023f32ba8
Aliaa-Ahmed <91092300+Aliaa-Ahmed@users.noreply.github.com>
2021-10-13 17:05:03 +0200

    .

---
+++ rootnet.py
@@ -0,0 +1,18 @@
+import torch
+from torch.nn.parallel.data_parallel import DataParallel
+from model import get_pose_net
+from config import cfg
+import os.path as osp
+
+class rootnet:
+    def lood_model(path):
+        model_path = './snapshot_%d.pth.tar' % int(18)
+        assert osp.exists(model_path), 'Cannot find model at ' + model_path
+        print('Load checkpoint from {}'.format(model_path))
+        model = get_pose_net(cfg, False)
+        model = DataParallel(model).cpu()
+        ckpt = torch.load(model_path)
+        model.load_state_dict(ckpt['network'])
+        model.eval();
+        return model
+
